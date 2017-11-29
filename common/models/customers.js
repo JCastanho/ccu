0908db3dffd76cef079d2c86a187befd8fda66d5
JCastanho <joao_cast@hotmail.com>
2017-11-29 16:20:53 +0000

    a minha versao do backend

---
+++ common/models/customers.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = function(Customers) {
+
+	Customers.username = function(customer_name ,cb){
+		Customers.find({where: {customer_name: customer_name}}, function (err, instance){
+			var response = instance;
+			cb(null, response);
+        	console.log(response);
+		})
+	}
+
+	Customers.remoteMethod (
+		'username' ,
+		{
+			http: {path: '/username', verb: 'get'},
+			accepts: {arg: 'customer_name', type: 'string', http: {source: 'query'}},
+			returns: [
+			{arg: 'customer_id', type: 'number'},
+			{arg: 'login', type: 'string'},
+			{arg: 'password', type: 'string'},
+			{arg: 'email', type: 'string'},
+			{arg: 'customer_name', type: 'string'},
+			{arg: 'customer_address', type: 'string'},
+			{arg: 'creditcard', type: 'string'},
+			{arg: 'lastlogin', type: 'date'}
+			]
+		}
+	);
+
+	Customers.packsForSale = function(customer_id, cb){
+		var filter = { include : [ 'energy'] };
+		var capp = Customers.app;
+		var e = capp.models.Energy;
+		e.find({where: {holder: null}}, function(err, instance) {
+			var response = instance;
+			cb(null, response);
+			console.log(response);
+		})
+		
+	}
+
+	Customers.remoteMethod(
+		'packsForSale',
+		{
+			http: {path: '/packsForSale', verb: 'get'},
+			accepts: {arg: 'customer_id', type: 'number', http: {source: 'query'}},
+			returns: [
+			{arg: 'energy_id', type: 'number'},
+			{arg: 'producer_id', type: 'number'},
+			{arg: 'quatity', type: 'string'},
+			{arg: 'packName', type: 'string'},
+			{arg: 'packDescript', type: 'string'},
+			{arg: 'posted_time', type: 'date'},
+			{arg: 'holder', type: 'number'}
+			]
+		}
+		);
+
+};
